5729902660cc69de5ec7c217fe5a520400188df8
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-10-15 13:49:32 -0300

    add tests

--- test/com/rafaelnacle/minefield/model/FieldTest.java
+++ test/com/rafaelnacle/minefield/model/FieldTest.java
@@ -1,5 +1,6 @@
 package com.rafaelnacle.minefield.model;
 
+import com.rafaelnacle.minefield.exception.ExplosionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -53,5 +54,79 @@ public class FieldTest {
         Field neighbour = new Field(1, 1);
         boolean result = field.addNeighbour(neighbour);
         assertFalse(result);
+    }
+
+    @Test
+    void testDefaultValueFlaggedAttribute() {
+        assertFalse(field.isFlagged());
+    }
+
+    @Test
+    void testToggleFlagging() {
+        field.toggleFlagging();
+        assertTrue(field.isFlagged());
+    }
+
+    @Test
+    void testToggleFlaggingTwoTimes() {
+        field.toggleFlagging();
+        field.toggleFlagging();
+        assertFalse(field.isFlagged());
+    }
+
+    @Test
+    void testOpenNotMinedNotFlagged() {
+        assertTrue(field.open());
+    }
+
+    @Test
+    void testOpenNotMinedFlagged() {
+        field.toggleFlagging();
+        assertFalse(field.open());
+    }
+
+    @Test
+    void testOpenMinedFlagged() {
+        field.toggleFlagging();
+        field.mine();
+        assertFalse(field.open());
+    }
+
+    @Test
+    void testOpenMinedNotFlagged() {
+        field.mine();
+
+        assertThrows(ExplosionException.class, () -> {
+            field.open();
+        });
+    }
+
+    @Test
+    void testOpenNeighbourhood1() {
+        Field field11 = new Field(1, 1);
+        Field field22 = new Field(2,2);
+
+        field22.addNeighbour(field11);
+
+        field.addNeighbour(field22);
+        field.open();
+
+        assertTrue(field22.isOpen() && field11.isOpen());
+    }
+
+    @Test
+    void testOpenNeighbourhood2() {
+        Field field11 = new Field(1, 1);
+        Field field12 = new Field(1, 1);
+        field12.mine();
+
+        Field field22 = new Field(2,2);
+        field22.addNeighbour(field11);
+        field22.addNeighbour(field12);
+
+        field.addNeighbour(field22);
+        field.open();
+
+        assertTrue(field22.isOpen() && field11.isClosed());
     }
 }
