e8b57f92eebc7b3a336675b5323dfc16ac0dedb0
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-10-22 16:41:27 -0300

    instance BoardConsole

--- src/com/rafaelnacle/minefield/App.java
+++ src/com/rafaelnacle/minefield/App.java
@@ -1,15 +1,11 @@
 package com.rafaelnacle.minefield;
 
 import com.rafaelnacle.minefield.model.Board;
+import com.rafaelnacle.minefield.view.BoardConsole;
 
 public class App {
     public static void main(String[] args) {
         Board board = new Board(6, 6,6);
-
-        board.open(3, 3);
-        board.toggleFlagging(4, 4);
-        board.toggleFlagging(4, 5);
-
-        System.out.println(board);
+        new BoardConsole(board);
     }
 }
