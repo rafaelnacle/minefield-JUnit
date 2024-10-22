d5a2c4f5d68c5a2eccb5f4d31927f041cf5f8dc7
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-10-22 16:41:53 -0300

    implementation of ExitException

---
+++ src/com/rafaelnacle/minefield/exception/ExitException.java
@@ -0,0 +1,8 @@
+package com.rafaelnacle.minefield.exception;
+
+import java.io.Serial;
+
+public class ExitException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1;
+}
